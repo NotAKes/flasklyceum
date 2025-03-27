b815c5ad0fc6fa3befc66ed7676b19850d693eec
NotAkes <134485643+NotAKes@users.noreply.github.com>
2025-03-27 22:36:57 +0300

    commit

--- sqalchemy/Еще один запрос/main.py
+++ sqalchemy/Еще один запрос/main.py
@@ -3,11 +3,12 @@ from flask import Flask
 app = Flask(__name__)
 global_init(input())
 db_sess = create_session()
-for departament in db_sess.query(Departament).filter(Departament.id == 1).all():
-    for member in departament.members:
+for departament in db_sess.query(Department).filter(Department.id == 1).all():
+    for member in departament.members.split(', '):
         work_size = 0
         for job in db_sess.query(Jobs).all():
-            if str(member) in job.collaborators:
-                work_size += job.work_size
-        if job > 25:
-            print(member.name, member.surname)
+            if int(member) in list(map(int, job.collaborators.split(', '))):
+                work_size += int(job.work_size)
+        if work_size > 25:
+            worker = db_sess.query(User).filter(User.id == int(member)).all()[0]
+            print(worker.name, worker.surname)
